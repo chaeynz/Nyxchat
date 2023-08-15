98a0653b00b6ce059edab1de14fd11cf57819956
HINSTANCE_hInstance <125285546+RandomInteger123@users.noreply.github.com>
2023-08-16 02:38:10 +0300

    Update Serializer.hpp
    Added serializeMessage()

--- NyxchatCore/include/engine/Serializer.hpp
+++ NyxchatCore/include/engine/Serializer.hpp
@@ -1,1 +1,11 @@
 // Serializer.hpp
+#include <iostream>
+
+#include "include/components/Message.hpp"
+class Serializer
+{
+  int serializeMessage(unsigned long long messageId, std::string messageContent, User* messageAuthor);
+};
+
+
+
